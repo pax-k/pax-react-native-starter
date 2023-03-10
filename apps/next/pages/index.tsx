b2140bcf8fb2f6b7de732b1eb2c8eaa61d5d4f0b
Santiago Buenahora <santib97@gmail.com>
2023-03-10 09:07:28 -0500

    Add server side props

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,3 +1,10 @@
 import { HomeScreen } from 'app/src/features/home/HomeScreen'
 
+export const getServerSideProps = async ({ req }: any) => {
+  // const { userId } = getAuth(req);
+  // const user = userId ? await clerkClient.users.getUser(userId) : undefined;
+  // return { props: { ...buildClerkProps(req, { user }) } };
+  return { props: {} }
+};
+
 export default HomeScreen
